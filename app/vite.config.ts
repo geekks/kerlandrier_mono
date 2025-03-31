4146af95ee119d13964e55764aa28f32a36be2ca
adcousin <adrien.cousin@lilo.org>
2025-03-31 08:49:11 +0200

    chore(app) tailwind setup

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  watch: {
-      usePolling: true,  // Enable polling for file changes in Docker
-  },
-  // server: {
-  //   watch: {
-  //     ignored: ['!**/node_modules/**'], // Exclude node_modules explicitly
-  //     usePolling: true, // Use polling instead of file system events
-  //     interval: 100,    // Polling interval in milliseconds
-  //   },
-  // },
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
