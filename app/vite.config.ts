d119da0305d46721bf0954a4f4112d08fea82c38
Jean-Baptiste Bailly <jean.baptiste.bailly@gmail.com>
2025-11-12 10:58:11 +0100

    fix: allowed hosts with vite

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -8,4 +8,11 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  preview: {
+    allowedHosts: [
+      'app.kerlandrier.cc',
+      'localhost',
+      '127.0.0.1'
+    ]
+  }
 })
