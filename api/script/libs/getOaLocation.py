c885f99c6c6a42122bff2ced7d4b9dee5715aacb
Jean-Baptiste Bailly <jean.baptiste.bailly@gmail.com>
2026-09-02 10:47:01 +0200

    up: add new location matching

--- api/script/libs/getOaLocation.py
+++ api/script/libs/getOaLocation.py
@@ -79,6 +79,7 @@ def get_or_create_oa_location(searched_location:str,
         ("le big bang bar route de benodet Quimper","Le BigBang Bar"),
         ("kériolet","Keriolet"),
         ("3 rue Vauban  Ville Close","Musée de la Pêche"),
+        ('Espace Jeunes Mjc Trégunc',"MJC Trégunc"),
     ]
         for old, new in replacement:
             searched_location = searched_location.replace(old, new)
