0e9cfe4636aaa4163bc075e17fd3553732ae8a6c
Jean-Baptiste Bailly <jean.baptiste.bailly@gmail.com>
2026-09-02 10:46:46 +0200

    up: adduid (independant script)

--- api/script/libs/addUID.py
+++ api/script/libs/addUID.py
@@ -7,55 +7,102 @@ from git import Repo
 git_root = Repo(search_parent_directories=True).working_tree_dir
 sys.path.insert(0,   os.path.abspath(  os.path.join(  git_root,'resources/python' ) ) )
 
-from InquirerPy import prompt, inquirer
-from InquirerPy.validator import PathValidator
-from InquirerPy.base.control import Choice
-from InquirerPy.separator import Separator
 import shutil, csv
-import pandas as pd
+import inquirer
+from configuration import config
 
+from HttpRequests import get_uid_from_name_date, get_events
 
-from utils import *
-from HttpRequests import *
-
+csv_path: inquirer.Path  = ""
+event_column_name: str = ""
+data_list_dict: list[dict]    = []
+validation: bool = False
 home_path = git_root if git_root else "~/"
-csv_path = inquirer.filepath(
-    message="Enter csv file path containing events wihtout their OA UID",
-    default= home_path + "/scraping/archives/2024_archipel_fouesnant/archipel_format.csv",
-    validate= PathValidator(is_file=True, message="Input is not a file")
-).execute()
-
-
-# create backup
-shutil.copyfile( csv_path, csv_path + ".bak", follow_symlinks=True)
 
 # Load CSV data to Dict
-try:
-    with open(csv_path, "r", encoding='utf-8') as file:
-        dictreader = csv.DictReader(file, delimiter=';')
-        headers = dictreader.fieldnames
-        data_dict = [ row for row in dictreader]
-except csv.Error as e:
-    print(f"Erreur lors de la lecture du fichier CSV : {e}")
-except Exception as e:
-    print(f"Une erreur s'est produite : {e}")
-    
-# Choose event name column and validate with first 3 rows
-eventname = inquirer.select(
-    message= "select a column containing the event name (to search it in OA events)",
-    choices= headers
-).execute()
+def read_csv_events(file_name_path:str) -> tuple[list[dict], list[str]]:
+    try:
+        with open(file_name_path, "r", encoding='utf-8') as file:
+            dictreader = csv.DictReader(file, delimiter=';')
+            csv_header = dictreader.fieldnames
+            event_list: list[dict] = [ row for row in dictreader]
+            
+            return event_list, csv_header
+    except csv.Error as e:
+        print(f"Erreur lors de la lecture du fichier CSV : {e}")
+    except Exception as e:
+        print(f"Une erreur s'est produite : {e}")
 
-if eventname:
-    validation = inquirer.confirm(
-    message="Is these data OK for looking for an event Title ? " + str( [entry[eventname] for entry in data_dict[:3] ] ) + " ...",
-    default= True,
-    ).execute()
 
-if (not validation) : exit(0)
+# Get first 3 values of a column in CSV to show it to user for validation
+def get_first_lines(event_column_name:str, data_list_dict:list[dict]) -> list[dict]:
+    try:
+        return [entry[event_column_name] for entry in data_list_dict[:3] ]
+    except Exception as e:
+        print(f"Une erreur s'est produite lors de l'extraction des données : {e}")
 
-for row in data_dict:
-    event_uid = get_uid_from_name_date(row[eventname], row['start_date'].split("T")[0])
-    existing_event=get_events(params={"uid": event_uid})['events'][0] if event_uid else None
-    existing_event_title = existing_event.get('title', {}).get('fr', "") if existing_event else "NOT FOUND"
-    print("event_name: " + row[eventname] + "---- existing event name----- " + str(existing_event_title))
+
+
+csv_path = inquirer.prompt(questions=[
+                inquirer.Path(
+                    name="csv_path",
+                    message="Enter csv file path containing events wihtout their OA UID",
+                    default= f"{home_path}/api/script/scraping/archipel_fouesnant/2024/archipel_format.csv",
+                    exists=True, 
+                    path_type=inquirer.Path.FILE
+                )
+            ]
+    )
+
+# create backup
+shutil.copyfile( csv_path['csv_path'], csv_path['csv_path'] + ".bak", follow_symlinks=True)
+
+# Première ligne du CSV pour récupérer les noms des colonnes
+csv_header = read_csv_events(csv_path['csv_path'])[1]
+
+# Liste des events
+event_list = read_csv_events(csv_path['csv_path'])[0]
+
+
+
+event_column_name  = inquirer.prompt( questions=[
+                    inquirer.List(
+                        name="event_column_name",
+                        message= "select a column containing the event name (to search it in OA events)",
+                        choices= csv_header
+                    )
+                ]
+        )
+
+# First titles events
+title_examples=get_first_lines(event_column_name['event_column_name'], event_list)
+title_examples_shorten = ",\n -".join(title_examples[:3])
+
+print(f"First 3 events in CSV file:\n -{title_examples_shorten}")
+
+validation = inquirer.prompt( questions=[
+                inquirer.Confirm(
+                    name="validation",
+                    message=f"Does it look correct?",
+                    default= True,
+                )
+            ]
+)
+
+
+if (not validation['validation']) : exit(0)
+
+
+for row in event_list:
+    event_uid = get_uid_from_name_date(
+                    row[event_column_name['event_column_name']],
+                    row['start_date'].split("T")[0]
+                    )
+    if event_uid:
+        existing_event = get_events(
+                            params={"uid": event_uid}
+                            )[0]
+    if existing_event:
+        existing_event_title = existing_event.get('title', {}).get('fr', "NOT FOUND")
+
+    print(f"event_name: {row[event_column_name['event_column_name']]} ---- existing event name----- {existing_event_title}")
